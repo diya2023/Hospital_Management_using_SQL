5dd40f2488491ec4c7a903304b86ff10355a2924
diya2023 <127024060+diya2023@users.noreply.github.com>
2023-09-14 10:15:54 +0530

    Add files via upload

---
+++ data_input.sql
@@ -0,0 +1,46 @@
+insert into employeenew values(1001,'diya','Ahmedabad',6543567812,'Doctor',40000,'B+');
+
+insert into employeenew values(1002,'nidhi','gandhinagar',9876543456,'Nurse',15000,'A-');
+
+insert into employee1 values(1003,'radhika','rajkot',9345543456,'reception',9000,'O-');
+
+insert into employeenew values(1004,'aaliya','Ahmedabad',6755543456,'doctor',42000,'O+');
+
+insert into employeenew values(1005,'khushi','amreli',9080343456,'Nurse',10000,'A+');
+
+insert into employeenew values(1006,'raj','surat',9543453456,'Doctor',30000,'B-');
+
+insert into patientnew values(2000,1001,'yagna','Ahemdabad',6767453456,'22/07/2021','14/08/2021','A+');
+
+insert into patientnew values(2001,1004,'dhairya','Goa',6578413456,'05/09/2021','06/02/2022','B+');
+
+insert into patientnew values(2002,1006,'Henil','Ahemdabad',9878906541,'13/05/2022','23/05/2022','O+');
+
+insert into doctornew values(1001,2000,'Diya','Cardiology');
+
+insert into doctornew values(1004,2001,'Aaliya','Surgery');
+
+insert into doctornew values(1006,2002,'Raj','Neurology');
+
+insert into nursenew values(1002,NULL,'Nidhi');
+
+insert into nursenew values(1005,NULL,'Khushi');
+
+insert into recordsnew values(100,2000,1001,'Doctor','Heartattack');
+
+insert into recordsnew values(101,2001,1004,'Doctor','Endoscopy');
+
+insert into recordsnew values(102,2002,1006,'Doctor','BrainSurgery');
+
+insert into roomnew values(200,100,'A.C Room',2000,1001,'Doctor');
+
+insert into receptionlistnew values(1003,'radhika');
+insert into roomnew values(201,111,'Normal',2001,1004,'Doctor');
+
+insert into roomnew values(202,110,'Delux',2002,1006,'Doctor');
+
+insert into medicinesnew values(400,2000,5,'Statins',5000);
+
+insert into medicinesnew values(401,2001,10,'Dolo',200);
+
+insert into medicinesnew values(402,2002,15,'Amantadine',200);
